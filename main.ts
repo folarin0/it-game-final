59338acd6d7a46acd86d31179cb120b48cc2b3ec
folarin0 <160610624+folarin0@users.noreply.github.com>
2024-03-06 09:48:44 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+scene.setBackgroundColor(14)
+let Player_1 = sprites.create(img`
+    . . . f f f f f f f f f f . . . 
+    . . . f f . . . . . . f f . . . 
+    . . . f . . . . f . . . f . . . 
+    . . . f . . . f . f . . f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . . f f . f f . . . . . 
+    . . . . . . f . . . f . . . . . 
+    . f f f f f f . . . f f f f f . 
+    . f . . . . f . . . f . . . f . 
+    . f . . . . f . . . f . . . f . 
+    f f f . . . f . . . f . . f f f 
+    f f f . . . f . . . f . . f f f 
+    . . . . . . f . . . f . . . . . 
+    . . . . . . f . . . f . . . . . 
+    . . . . . f f . . . f f . . . . 
+    . . . . . f f . . . f f . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(Player_1, 100, 100)
+tiles.setCurrentTilemap(tilemap`level1`)
---
+++ tilemap.g.ts
@@ -0,0 +1,47 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000002000002000000000000000000000002000000000200000000000000000002010101010101020000000000000000000000000000000000000000000000020000000000000000020000000000000202020202020202020200000002020202020202020202020202020202020100010202020200000000020202000000020000000002000100010000010102010102000002000000020000020002000000000000000000000000000000000000000000000000000000000000000000000100000001000100010000000100010000010100000100000000010101010100010101010001010101010001000101010101010101010101010101010101`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
