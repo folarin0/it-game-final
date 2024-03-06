f208abcf4072481675c664314e5caebd5f40addb
folarin0 <160610624+folarin0@users.noreply.github.com>
2024-03-06 19:38:31 +0000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -3,27 +3,21 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         Player_1.vy = -150
     }
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    controller.moveSprite(enemy_1, 100, 0)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.melt)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Player_1.vx = 50
-    Player_1.setImage(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . f f f f f f f f . . . . . . 
-        . . f f . . . . f f . . . . . . 
-        . . f . . . f . . f . . . . . . 
-        . . f . . f . f . f . . . . . . 
-        . . f f f f f f f f . . . . . . 
-        . . . f f . . f . . . . . . . . 
-        f f f f . . . . f f . . . . . . 
-        f . f f . . f f f f . . . . . . 
-        f f f . . . f . . f . . . . . . 
-        f f f . . . f . . f f . . . . . 
-        . f f . . f f f . . f . . . . . 
-        . f . . . f f f . . f f . . . . 
-        f f . . . . . . . . . f f . . . 
-        f f . . . . . . . . . f f . . . 
-        `)
 })
+scene.onOverlapTile(SpriteKind.Enemy, assets.tile`myTile`, function (sprite, location) {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
+})
+let enemy_1: Sprite = null
 let Player_1: Sprite = null
 scene.setBackgroundColor(14)
 Player_1 = sprites.create(img`
@@ -47,27 +41,25 @@ Player_1 = sprites.create(img`
 controller.moveSprite(Player_1, 100, 0)
 Player_1.ay = 500
 tiles.setCurrentTilemap(tilemap`level1`)
+scene.cameraFollowSprite(Player_1)
 game.onUpdate(function () {
-    Player_1.setImage(img`
-        . . . f f f f f f f f f f . . . 
-        . . . f f . . . . . . f f . . . 
-        . . . f . . . . f . . . f . . . 
-        . . . f . . . f . f . . f . . . 
-        . . . f f f f f f f f f f . . . 
-        . . . . . . f f . f f . . . . . 
-        . . . . . . f . . . f . . . . . 
-        . f f f f f f . . . f f f f f . 
-        . f . . . . f . . . f . . . f . 
-        . f . . . . f . . . f . . . f . 
-        f f f . . . f . . . f . . f f f 
-        f f f . . . f . . . f . . f f f 
-        . . . . . . f . . . f . . . . . 
-        . . . . . . f . . . f . . . . . 
-        . . . . . f f . . . f f . . . . 
-        . . . . . f f . . . f f . . . . 
-        `)
-    controller.moveSprite(Player_1, 100, 0)
-    if (Player_1.ax < 0) {
-    	
-    }
+    enemy_1 = sprites.create(img`
+        . . . 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . . 7 2 . . . . . . 2 7 . . . 
+        . . . 7 . . . . . . . . 7 . . . 
+        . . . 7 . . . 7 . . . . 7 . . . 
+        . . . 7 7 . 7 . 7 . . 7 7 . . . 
+        . . . 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . . . 2 2 . . . . 2 2 . . . . 
+        . . . . 2 . . . . . . 2 . . . . 
+        2 2 2 2 2 . . . . . . 2 2 2 2 2 
+        2 . . . 2 . . . . . . 2 . . . 2 
+        2 . . . 2 . . . . . . 2 . . . 2 
+        7 2 . . 2 . . . . . . 2 . . 2 7 
+        2 7 . . 2 . . . . . . 2 . . 7 2 
+        . . . . 2 . . . . . . 2 . . . . 
+        . . . 7 2 . . . . . . 2 7 . . . 
+        . . . 2 7 . . . . . . 7 2 . . . 
+        `, SpriteKind.Enemy)
+    enemy_1.setPosition(134, 70)
 })
