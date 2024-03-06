74aeb41bf98959c694e2b15246849bcb8e583040
folarin0 <160610624+folarin0@users.noreply.github.com>
2024-03-06 12:20:08 +0000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,32 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Player_1.vy == 0) {
+        Player_1.vy = -150
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Player_1.vx = 50
+    Player_1.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . f f f f f f f f . . . . . . 
+        . . f f . . . . f f . . . . . . 
+        . . f . . . f . . f . . . . . . 
+        . . f . . f . f . f . . . . . . 
+        . . f f f f f f f f . . . . . . 
+        . . . f f . . f . . . . . . . . 
+        f f f f . . . . f f . . . . . . 
+        f . f f . . f f f f . . . . . . 
+        f f f . . . f . . f . . . . . . 
+        f f f . . . f . . f f . . . . . 
+        . f f . . f f f . . f . . . . . 
+        . f . . . f f f . . f f . . . . 
+        f f . . . . . . . . . f f . . . 
+        f f . . . . . . . . . f f . . . 
+        `)
+})
+let Player_1: Sprite = null
 scene.setBackgroundColor(14)
-let Player_1 = sprites.create(img`
+Player_1 = sprites.create(img`
     . . . f f f f f f f f f f . . . 
     . . . f f . . . . . . f f . . . 
     . . . f . . . . f . . . f . . . 
@@ -17,5 +44,30 @@ let Player_1 = sprites.create(img`
     . . . . . f f . . . f f . . . . 
     . . . . . f f . . . f f . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(Player_1, 100, 100)
+controller.moveSprite(Player_1, 100, 0)
+Player_1.ay = 500
 tiles.setCurrentTilemap(tilemap`level1`)
+game.onUpdate(function () {
+    Player_1.setImage(img`
+        . . . f f f f f f f f f f . . . 
+        . . . f f . . . . . . f f . . . 
+        . . . f . . . . f . . . f . . . 
+        . . . f . . . f . f . . f . . . 
+        . . . f f f f f f f f f f . . . 
+        . . . . . . f f . f f . . . . . 
+        . . . . . . f . . . f . . . . . 
+        . f f f f f f . . . f f f f f . 
+        . f . . . . f . . . f . . . f . 
+        . f . . . . f . . . f . . . f . 
+        f f f . . . f . . . f . . f f f 
+        f f f . . . f . . . f . . f f f 
+        . . . . . . f . . . f . . . . . 
+        . . . . . . f . . . f . . . . . 
+        . . . . . f f . . . f f . . . . 
+        . . . . . f f . . . f f . . . . 
+        `)
+    controller.moveSprite(Player_1, 100, 0)
+    if (Player_1.ax < 0) {
+    	
+    }
+})
