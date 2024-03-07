c3480c4ce46be795429eb6842554cbd03da605de
folarin0 <160610624+folarin0@users.noreply.github.com>
2024-03-07 12:19:08 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,15 +3,15 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         Player_1.vy = -150
     }
 })
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    controller.moveSprite(enemy_1, 100, 0)
-})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     game.gameOver(false)
     game.setGameOverEffect(false, effects.melt)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Player_1.vx = 50
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
 })
 scene.onOverlapTile(SpriteKind.Enemy, assets.tile`myTile`, function (sprite, location) {
     game.gameOver(true)
@@ -43,6 +43,9 @@ Player_1.ay = 500
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(Player_1)
 game.onUpdate(function () {
+    info.setLife(10)
+})
+game.onUpdateInterval(600, function () {
     enemy_1 = sprites.create(img`
         . . . 7 7 7 7 7 7 7 7 7 7 . . . 
         . . . 7 2 . . . . . . 2 7 . . . 
@@ -62,4 +65,23 @@ game.onUpdate(function () {
         . . . 2 7 . . . . . . 7 2 . . . 
         `, SpriteKind.Enemy)
     enemy_1.setPosition(134, 70)
+    enemy_1 = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, enemy_1, 100, 0)
+    enemy_1.follow(Player_1, 110)
 })
