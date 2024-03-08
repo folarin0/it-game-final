e8e6a12c86f543eb6523c90fe36b825ce22c4d4d
folarin0 <160610624+folarin0@users.noreply.github.com>
2024-03-08 15:39:29 +0000

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,110 @@
+def on_up_pressed():
+    if Player_1.vy == 0:
+        Player_1.vy = -150
+controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+
+def on_overlap_tile(sprite, location):
+    game.game_over(False)
+    game.set_game_over_effect(False, effects.melt)
+scene.on_overlap_tile(SpriteKind.player,
+    assets.tile("""
+        myTile
+    """),
+    on_overlap_tile)
+
+def on_a_pressed():
+    Player_1.vx = 50
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+def on_on_overlap(sprite2, otherSprite):
+    sprites.destroy(sprite2)
+    info.change_life_by(-1)
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.player, on_on_overlap)
+
+def on_overlap_tile2(sprite3, location2):
+    game.game_over(True)
+    game.set_game_over_effect(True, effects.confetti)
+scene.on_overlap_tile(SpriteKind.enemy,
+    assets.tile("""
+        myTile
+    """),
+    on_overlap_tile2)
+
+enemy_1: Sprite = None
+Player_1: Sprite = None
+scene.set_background_color(14)
+Player_1 = sprites.create(img("""
+        . . . f f f f f f f f f f . . . 
+            . . . f f . . . . . . f f . . . 
+            . . . f . . . . f . . . f . . . 
+            . . . f . . . f . f . . f . . . 
+            . . . f f f f f f f f f f . . . 
+            . . . . . . f f . f f . . . . . 
+            . . . . . . f . . . f . . . . . 
+            . f f f f f f . . . f f f f f . 
+            . f . . . . f . . . f . . . f . 
+            . f . . . . f . . . f . . . f . 
+            f f f . . . f . . . f . . f f f 
+            f f f . . . f . . . f . . f f f 
+            . . . . . . f . . . f . . . . . 
+            . . . . . . f . . . f . . . . . 
+            . . . . . f f . . . f f . . . . 
+            . . . . . f f . . . f f . . . .
+    """),
+    SpriteKind.player)
+controller.move_sprite(Player_1, 100, 0)
+Player_1.ay = 500
+tiles.set_current_tilemap(tilemap("""
+    level1
+"""))
+scene.camera_follow_sprite(Player_1)
+
+def on_on_update():
+    info.set_life(10)
+game.on_update(on_on_update)
+
+def on_update_interval():
+    global enemy_1
+    enemy_1 = sprites.create(img("""
+            . . . 7 7 7 7 7 7 7 7 7 7 . . . 
+                    . . . 7 2 . . . . . . 2 7 . . . 
+                    . . . 7 . . . . . . . . 7 . . . 
+                    . . . 7 . . . 7 . . . . 7 . . . 
+                    . . . 7 7 . 7 . 7 . . 7 7 . . . 
+                    . . . 7 7 7 7 7 7 7 7 7 7 . . . 
+                    . . . . 2 2 . . . . 2 2 . . . . 
+                    . . . . 2 . . . . . . 2 . . . . 
+                    2 2 2 2 2 . . . . . . 2 2 2 2 2 
+                    2 . . . 2 . . . . . . 2 . . . 2 
+                    2 . . . 2 . . . . . . 2 . . . 2 
+                    7 2 . . 2 . . . . . . 2 . . 2 7 
+                    2 7 . . 2 . . . . . . 2 . . 7 2 
+                    . . . . 2 . . . . . . 2 . . . . 
+                    . . . 7 2 . . . . . . 2 7 . . . 
+                    . . . 2 7 . . . . . . 7 2 . . .
+        """),
+        SpriteKind.enemy)
+    enemy_1.set_position(134, 70)
+    enemy_1 = sprites.create_projectile_from_sprite(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        enemy_1,
+        100,
+        0)
+    enemy_1.follow(Player_1, 110)
+game.on_update_interval(600, on_update_interval)
--- main.ts
+++ main.ts
@@ -3,6 +3,10 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         Player_1.vy = -150
     }
 })
+scene.onOverlapTile(SpriteKind.Enemy, assets.tile`myTile`, function (sprite3, location2) {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     game.gameOver(false)
     game.setGameOverEffect(false, effects.melt)
@@ -11,11 +15,10 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Player_1.vx = 50
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
-    sprites.destroy(sprite)
+    info.changeLifeBy(-1)
 })
-scene.onOverlapTile(SpriteKind.Enemy, assets.tile`myTile`, function (sprite, location) {
-    game.gameOver(true)
-    game.setGameOverEffect(true, effects.confetti)
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite2, otherSprite) {
+    sprites.destroy(sprite2)
 })
 let enemy_1: Sprite = null
 let Player_1: Sprite = null
@@ -42,9 +45,7 @@ controller.moveSprite(Player_1, 100, 0)
 Player_1.ay = 500
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(Player_1)
-game.onUpdate(function () {
-    info.setLife(10)
-})
+info.setLife(10)
 game.onUpdateInterval(600, function () {
     enemy_1 = sprites.create(img`
         . . . 7 7 7 7 7 7 7 7 7 7 . . . 
