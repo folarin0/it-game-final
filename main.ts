bc4e2171723baf33b9e828974737f31c94d51afe
folarin0 <160610624+folarin0@users.noreply.github.com>
2024-03-11 10:58:01 +0000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const laser = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Player_1.vy == 0) {
         Player_1.vy = -150
@@ -12,13 +15,35 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, lo
     game.setGameOverEffect(false, effects.melt)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    Player_1.vx = 50
+    Player_1.setKind(SpriteKind.laser)
+    enemy_1 = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . 2 2 2 2 2 2 2 . . . . 
+        . . . . 2 2 2 2 2 2 2 2 2 . . . 
+        . . . 2 2 2 2 2 2 2 2 2 2 2 . . 
+        . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+        . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+        . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+        . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+        . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+        . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+        . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+        . . . 2 2 2 2 2 2 2 2 2 2 2 . . 
+        . . . . 2 2 2 2 2 2 2 2 2 . . . 
+        . . . . . 2 2 2 2 2 2 2 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, Player_1, 100, 0)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
+    Player_1.setVelocity(-100, 0)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite2, otherSprite) {
     sprites.destroy(sprite2)
+})
+sprites.onOverlap(SpriteKind.laser, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
 })
 let enemy_1: Sprite = null
 let Player_1: Sprite = null
@@ -46,7 +71,7 @@ Player_1.ay = 500
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(Player_1)
 info.setLife(10)
-game.onUpdateInterval(600, function () {
+game.onUpdateInterval(1000, function () {
     enemy_1 = sprites.create(img`
         . . . 7 7 7 7 7 7 7 7 7 7 . . . 
         . . . 7 2 . . . . . . 2 7 . . . 
