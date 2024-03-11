0623639e521a80b365140fd206c92319be027f54
folarin0 <160610624+folarin0@users.noreply.github.com>
2024-03-11 10:06:10 +0000

    Restore '3/8/2024, 3:39:29 PM Update pxt.json, main.blocks, main.ts, main.py'

--- main.ts
+++ main.ts
@@ -1,96 +1,88 @@
-controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Player_1.vy == 0) {
         Player_1.vy = -150
     }
-    
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`
-        myTile
-    `, function on_overlap_tile(sprite: Sprite, location: tiles.Location) {
-    game.gameOver(false)
-    game.setGameOverEffect(false, effects.melt)
-})
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
-    Player_1.vx = 50
-})
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function on_on_overlap(sprite2: Sprite, otherSprite: Sprite) {
-    sprites.destroy(sprite2)
-    info.changeLifeBy(-1)
-})
-scene.onOverlapTile(SpriteKind.Enemy, assets.tile`
-        myTile
-    `, function on_overlap_tile2(sprite3: Sprite, location2: tiles.Location) {
+scene.onOverlapTile(SpriteKind.Enemy, assets.tile`myTile`, function (sprite3, location2) {
     game.gameOver(true)
     game.setGameOverEffect(true, effects.confetti)
 })
-let enemy_1 : Sprite = null
-let Player_1 : Sprite = null
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.melt)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Player_1.vx = 50
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite2, otherSprite) {
+    sprites.destroy(sprite2)
+})
+let enemy_1: Sprite = null
+let Player_1: Sprite = null
 scene.setBackgroundColor(14)
 Player_1 = sprites.create(img`
-        . . . f f f f f f f f f f . . . 
-            . . . f f . . . . . . f f . . . 
-            . . . f . . . . f . . . f . . . 
-            . . . f . . . f . f . . f . . . 
-            . . . f f f f f f f f f f . . . 
-            . . . . . . f f . f f . . . . . 
-            . . . . . . f . . . f . . . . . 
-            . f f f f f f . . . f f f f f . 
-            . f . . . . f . . . f . . . f . 
-            . f . . . . f . . . f . . . f . 
-            f f f . . . f . . . f . . f f f 
-            f f f . . . f . . . f . . f f f 
-            . . . . . . f . . . f . . . . . 
-            . . . . . . f . . . f . . . . . 
-            . . . . . f f . . . f f . . . . 
-            . . . . . f f . . . f f . . . .
+    . . . f f f f f f f f f f . . . 
+    . . . f f . . . . . . f f . . . 
+    . . . f . . . . f . . . f . . . 
+    . . . f . . . f . f . . f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . . f f . f f . . . . . 
+    . . . . . . f . . . f . . . . . 
+    . f f f f f f . . . f f f f f . 
+    . f . . . . f . . . f . . . f . 
+    . f . . . . f . . . f . . . f . 
+    f f f . . . f . . . f . . f f f 
+    f f f . . . f . . . f . . f f f 
+    . . . . . . f . . . f . . . . . 
+    . . . . . . f . . . f . . . . . 
+    . . . . . f f . . . f f . . . . 
+    . . . . . f f . . . f f . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(Player_1, 100, 0)
 Player_1.ay = 500
-tiles.setCurrentTilemap(tilemap`
-    level1
-`)
+tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(Player_1)
-game.onUpdate(function on_on_update() {
-    info.setLife(10)
-})
-game.onUpdateInterval(600, function on_update_interval() {
-    
+info.setLife(10)
+game.onUpdateInterval(600, function () {
     enemy_1 = sprites.create(img`
-            . . . 7 7 7 7 7 7 7 7 7 7 . . . 
-                    . . . 7 2 . . . . . . 2 7 . . . 
-                    . . . 7 . . . . . . . . 7 . . . 
-                    . . . 7 . . . 7 . . . . 7 . . . 
-                    . . . 7 7 . 7 . 7 . . 7 7 . . . 
-                    . . . 7 7 7 7 7 7 7 7 7 7 . . . 
-                    . . . . 2 2 . . . . 2 2 . . . . 
-                    . . . . 2 . . . . . . 2 . . . . 
-                    2 2 2 2 2 . . . . . . 2 2 2 2 2 
-                    2 . . . 2 . . . . . . 2 . . . 2 
-                    2 . . . 2 . . . . . . 2 . . . 2 
-                    7 2 . . 2 . . . . . . 2 . . 2 7 
-                    2 7 . . 2 . . . . . . 2 . . 7 2 
-                    . . . . 2 . . . . . . 2 . . . . 
-                    . . . 7 2 . . . . . . 2 7 . . . 
-                    . . . 2 7 . . . . . . 7 2 . . .
+        . . . 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . . 7 2 . . . . . . 2 7 . . . 
+        . . . 7 . . . . . . . . 7 . . . 
+        . . . 7 . . . 7 . . . . 7 . . . 
+        . . . 7 7 . 7 . 7 . . 7 7 . . . 
+        . . . 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . . . 2 2 . . . . 2 2 . . . . 
+        . . . . 2 . . . . . . 2 . . . . 
+        2 2 2 2 2 . . . . . . 2 2 2 2 2 
+        2 . . . 2 . . . . . . 2 . . . 2 
+        2 . . . 2 . . . . . . 2 . . . 2 
+        7 2 . . 2 . . . . . . 2 . . 2 7 
+        2 7 . . 2 . . . . . . 2 . . 7 2 
+        . . . . 2 . . . . . . 2 . . . . 
+        . . . 7 2 . . . . . . 2 7 . . . 
+        . . . 2 7 . . . . . . 7 2 . . . 
         `, SpriteKind.Enemy)
     enemy_1.setPosition(134, 70)
     enemy_1 = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
         `, enemy_1, 100, 0)
     enemy_1.follow(Player_1, 110)
 })
