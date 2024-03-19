6d1cc32965913810a5a16bcf78595e7c098a75c5
folarin0 <160610624+folarin0@users.noreply.github.com>
2024-03-19 13:21:04 +0000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
+@namespace
+class SpriteKind:
+    laser = SpriteKind.create()
+
 def on_up_pressed():
     if Player_1.vy == 0:
         Player_1.vy = -150
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
-def on_overlap_tile(sprite, location):
-    game.game_over(False)
-    game.set_game_over_effect(False, effects.melt)
-scene.on_overlap_tile(SpriteKind.player,
-    assets.tile("""
-        myTile
-    """),
-    on_overlap_tile)
-
-def on_a_pressed():
-    Player_1.vx = 50
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
-
-def on_on_overlap(sprite2, otherSprite):
-    sprites.destroy(sprite2)
-    info.change_life_by(-1)
-sprites.on_overlap(SpriteKind.projectile, SpriteKind.player, on_on_overlap)
-
-def on_overlap_tile2(sprite3, location2):
+def on_overlap_tile(sprite3, location2):
     game.game_over(True)
     game.set_game_over_effect(True, effects.confetti)
 scene.on_overlap_tile(SpriteKind.enemy,
     assets.tile("""
         myTile
     """),
+    on_overlap_tile)
+
+def on_overlap_tile2(sprite, location):
+    game.game_over(False)
+    game.set_game_over_effect(False, effects.melt)
+scene.on_overlap_tile(SpriteKind.player,
+    assets.tile("""
+        myTile
+    """),
     on_overlap_tile2)
+
+def on_a_pressed():
+    global enemy_1
+    Player_1.set_kind(SpriteKind.laser)
+    enemy_1 = sprites.create_projectile_from_sprite(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . 2 2 2 2 2 2 2 . . . . 
+                    . . . . 2 2 2 2 2 2 2 2 2 . . . 
+                    . . . 2 2 2 2 2 2 2 2 2 2 2 . . 
+                    . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+                    . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+                    . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+                    . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+                    . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+                    . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+                    . . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+                    . . . 2 2 2 2 2 2 2 2 2 2 2 . . 
+                    . . . . 2 2 2 2 2 2 2 2 2 . . . 
+                    . . . . . 2 2 2 2 2 2 2 . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        Player_1,
+        100,
+        0)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+def on_on_overlap(sprite2, otherSprite):
+    Player_1.set_velocity(-100, 0)
+    info.change_life_by(-1)
+    sprites.destroy(enemy_1)
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.player, on_on_overlap)
+
+def on_on_overlap2(sprite4, otherSprite2):
+    sprites.destroy(otherSprite2)
+sprites.on_overlap(SpriteKind.laser, SpriteKind.enemy, on_on_overlap2)
 
 enemy_1: Sprite = None
 Player_1: Sprite = None
@@ -58,10 +89,7 @@ tiles.set_current_tilemap(tilemap("""
     level1
 """))
 scene.camera_follow_sprite(Player_1)
-
-def on_on_update():
-    info.set_life(10)
-game.on_update(on_on_update)
+info.set_life(10)
 
 def on_update_interval():
     global enemy_1
@@ -107,4 +135,4 @@ def on_update_interval():
         100,
         0)
     enemy_1.follow(Player_1, 110)
-game.on_update_interval(600, on_update_interval)
+game.on_update_interval(1000, on_update_interval)
