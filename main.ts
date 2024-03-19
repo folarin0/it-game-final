b75ea37d24f8c5282e20d2db66073e30d18eb374
folarin0 <160610624+folarin0@users.noreply.github.com>
2024-03-19 12:36:29 +0000

    Restore '3/14/2024, 11:50:44 PM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 namespace SpriteKind {
     export const enemssss = SpriteKind.create()
-    export const playerlaser = SpriteKind.create()
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Player_1.vy == 0) {
@@ -8,8 +7,7 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    Player_1 = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
+    enemy_1 = sprites.createProjectileFromSprite(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . 2 2 2 2 2 2 2 . . . . . 
@@ -25,11 +23,14 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        `, SpriteKind.playerlaser)
+        . . . . . . . . . . . . . . . . 
+        `, Player_1, 50, 0)
 })
 sprites.onOverlap(SpriteKind.enemssss, SpriteKind.Player, function (sprite, otherSprite) {
-	
+    sprites.destroy(sprite)
+    sprites.destroy(enemy_1)
 })
+let enemy_1: Sprite = null
 let Player_1: Sprite = null
 scene.setBackgroundColor(14)
 Player_1 = sprites.create(img`
@@ -56,5 +57,42 @@ tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(Player_1)
 info.setLife(10)
 game.onUpdateInterval(1000, function () {
-	
+    enemy_1 = sprites.create(img`
+        . . . 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . . 7 2 . . . . . . 2 7 . . . 
+        . . . 7 . . . . . . . . 7 . . . 
+        . . . 7 . . . 7 . . . . 7 . . . 
+        . . . 7 7 . 7 . 7 . . 7 7 . . . 
+        . . . 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . . . 2 2 . . . . 2 2 . . . . 
+        . . . . 2 . . . . . . 2 . . . . 
+        2 2 2 2 2 . . . . . . 2 2 2 2 2 
+        2 . . . 2 . . . . . . 2 . . . 2 
+        2 . . . 2 . . . . . . 2 . . . 2 
+        7 2 . . 2 . . . . . . 2 . . 2 7 
+        2 7 . . 2 . . . . . . 2 . . 7 2 
+        . . . . 2 . . . . . . 2 . . . . 
+        . . . 7 2 . . . . . . 2 7 . . . 
+        . . . 2 7 . . . . . . 7 2 . . . 
+        `, SpriteKind.Enemy)
+    enemy_1.setPosition(134, 70)
+    enemy_1.setKind(SpriteKind.enemssss)
+    Player_1 = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, enemy_1, -500, 0)
 })
