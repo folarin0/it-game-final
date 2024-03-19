28cf8cca7bc3a0eaabba40e6574d41189f1f9266
folarin0 <160610624+folarin0@users.noreply.github.com>
2024-03-19 12:43:40 +0000

    Restore '3/6/2024, 7:38:31 PM Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts'

--- main.ts
+++ main.ts
@@ -1,35 +1,21 @@
-namespace SpriteKind {
-    export const enemssss = SpriteKind.create()
-}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Player_1.vy == 0) {
         Player_1.vy = -150
     }
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.enemssss, function (sprite, otherSprite) {
-    sprites.destroy(sprite)
-    Player_1.setPosition(16, 56)
-    info.changeLifeBy(-1)
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    controller.moveSprite(enemy_1, 100, 0)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.melt)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    enemy_1 = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . 2 2 2 2 2 2 2 . . . . . 
-        . . . 2 2 2 2 2 2 2 2 2 . . . . 
-        . . . 2 2 2 2 2 2 2 2 2 . . . . 
-        . . . 2 2 2 2 2 2 2 2 2 . . . . 
-        . . . 2 2 2 2 2 2 2 2 2 . . . . 
-        . . . 2 2 2 2 2 2 2 2 2 . . . . 
-        . . . 2 2 2 2 2 2 2 2 2 . . . . 
-        . . . 2 2 2 2 2 2 2 2 2 . . . . 
-        . . . . 2 2 2 2 2 2 2 . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, Player_1, 50, 0)
+    Player_1.vx = 50
+})
+scene.onOverlapTile(SpriteKind.Enemy, assets.tile`myTile`, function (sprite, location) {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
 })
 let enemy_1: Sprite = null
 let Player_1: Sprite = null
@@ -56,8 +42,7 @@ controller.moveSprite(Player_1, 100, 0)
 Player_1.ay = 500
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(Player_1)
-info.setLife(10)
-game.onUpdateInterval(1000, function () {
+game.onUpdate(function () {
     enemy_1 = sprites.create(img`
         . . . 7 7 7 7 7 7 7 7 7 7 . . . 
         . . . 7 2 . . . . . . 2 7 . . . 
@@ -77,23 +62,4 @@ game.onUpdateInterval(1000, function () {
         . . . 2 7 . . . . . . 7 2 . . . 
         `, SpriteKind.Enemy)
     enemy_1.setPosition(134, 70)
-    Player_1 = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, enemy_1, 50, 50)
-    enemy_1.follow(Player_1, 110)
 })
